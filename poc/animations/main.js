7a5e6cb29f754d3e17f406efcb526fa336da0688
Luc Swinkels <luc.swinkels@live.nl>
2020-10-11 21:45:40 +0200

    added poc for vanilla js animations with working fade-up animation and core html/css

---
+++ poc/animations/main.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    let animations = document.querySelectorAll(".animated");
+    window.addEventListener('scroll', fadeIn);
+
+    function fadeIn() {
+        for (var i = 0; i < animations.length; i++) {
+            var element = animations[i]
+            var visible = element.getBoundingClientRect().top - window.innerHeight + 20;
+            if (visible < 0) {
+                element.classList.add("visible");
+            } else {
+                element.classList.remove("visible");
+            }
+        }
+    }
+    fadeIn();
+};
