9d93306140e6bf13dd49d6edcf69391956bbece3
Luc Swinkels <luc.swinkels@live.nl>
2020-12-02 18:00:25 +0100

    added logo to preloader in html svg format, defined elements to be animated and added basic moving animation

--- poc/advanced-development/preloader/src/js/main.js
+++ poc/advanced-development/preloader/src/js/main.js
@@ -1,1 +1,17 @@
-window.onload = function () {}
+window.onload = function () {
+    // Elementen die beinvloed worden definiëren
+    var elements = document.querySelectorAll('.logo circle');
+
+    var preloader = anime({
+        // Definiëren welke elementen geanimeerd worden
+        targets: elements,
+        // Animatie properties
+        translateX: 50,
+        easing: 'easeInOutSine',
+        duration: 500,
+        // Manier van animeren zodat het herhalen soepel verloopt
+        direction: 'alternate',
+        // Oneindig herhalen
+        loop: true
+    });
+}
